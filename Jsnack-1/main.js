2e168c497ba6e8bd93607ce00cbc7c177ad6b41f
Giacomo <giacomo.1936@gmail.com>
2020-11-02 16:16:13 +0100

    initial commit

---
+++ Jsnack-1/main.js
@@ -0,0 +1,31 @@
+//Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore.
+
+var bici_leggera = [];
+var bici = [
+    {
+        nome: 'bici-uno',
+        peso: 5
+    },
+    {
+        nome: 'bici-due',
+        peso: 6
+    },
+    {
+        nome: 'bici-tre',
+        peso: 4
+    },
+    {
+        nome: 'bici-quattro',
+        peso: 6
+    },
+    {
+        nome: 'bici-cinque',
+        peso: 5
+    }
+]
+
+
+for (var i = 0; i < bici.length; i++) {
+    console.log(bici.peso);
+};
