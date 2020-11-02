6f45b4a91bcf84b44e43f65e2febb76651309c5d
Giacomo <giacomo.1936@gmail.com>
2020-11-02 17:22:46 +0100

    creato ciclo for per verificare quale bici abbia il peso minore

--- Jsnack-1/main.js
+++ Jsnack-1/main.js
@@ -1,31 +1,39 @@
 //Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 //Stampare a schermo la bici con peso minore.
+$(document).ready(function() {
+    var bici = [
+        {
+            nome: 'bici-uno',
+            peso: 5
+        },
+        {
+            nome: 'bici-due',
+            peso: 6
+        },
+        {
+            nome: 'bici-tre',
+            peso: 3
+        },
+        {
+            nome: 'bici-quattro',
+            peso: 6
+        },
+        {
+            nome: 'bici-cinque',
+            peso: 5
+        }
+    ]
 
-var bici_leggera = [];
-var bici = [
-    {
-        nome: 'bici-uno',
-        peso: 5
-    },
-    {
-        nome: 'bici-due',
-        peso: 6
-    },
-    {
-        nome: 'bici-tre',
-        peso: 4
-    },
-    {
-        nome: 'bici-quattro',
-        peso: 6
-    },
-    {
-        nome: 'bici-cinque',
-        peso: 5
+    var numero_minimo = Math.min(5, 6, 3, 6, 5);
+    console.log(numero_minimo);
+
+    for (var i = 0; i < bici.length; i++) {
+        console.log(bici[i].peso);
+        if (bici[i].peso == numero_minimo) {
+            console.log('è la bici con il peso minore');
+        }
+        else {
+            console.log('non è la bici con il peso minore');
+        }
     }
-]
-
-
-for (var i = 0; i < bici.length; i++) {
-    console.log(bici.peso);
-};
+});
